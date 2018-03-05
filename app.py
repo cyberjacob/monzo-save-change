b0042cecbe3fd3bd4014aa2bed7e725ed3bfd109
root <root@palindrome.home.jacobmansfield.co.uk>
2018-03-05 11:10:56 +0000

    Flask basics

---
+++ app.py
@@ -0,0 +1,17 @@
+import os
+
+from flask import Flask, request, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def index():
+    return "Hello, World!"
+
+@app.route('/webhook')
+def webhook():
+    return "OK"
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
