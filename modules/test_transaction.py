9eca90a56a9004544613ae540f77a6deec687965
root <root@palindrome.home.jacobmansfield.co.uk>
2018-03-06 10:42:43 +0000

    update config form

--- modules/test_transaction.py
+++ modules/test_transaction.py
@@ -1,2 +1,5 @@
+import json
+
 def webhook(monzo, data):
+    print(json.dumps(data))
     monzo.create_feed_item(data['data']['account_id'], "Transaction pushed!", "http://www.nyan.cat/cats/original.gif", "{0} to {1}".format(data['data']['amount'], data['data']['description']))
