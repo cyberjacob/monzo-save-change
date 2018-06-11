d85e614979e7aa6eeaeb89ba9f25afdf44cf38c5
Jacob Mansfield <jacob.mansfield@itvet.co.uk>
2018-06-11 15:23:14 +0100

    fix build

--- monzohosting/settings.py
+++ monzohosting/settings.py
@@ -81,15 +81,15 @@ TEMPLATES = [
 WSGI_APPLICATION = 'monzohosting.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/2.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# # Database
+# # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
+#
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 
 # Password validation
