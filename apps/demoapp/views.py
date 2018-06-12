e49cc9f4b325f0ce278ad90f60a5cdd1b1739edc
Jacob Mansfield <jacob.mansfield@itvet.co.uk>
2018-06-12 13:55:40 +0100

    fix build

--- apps/demoapp/views.py
+++ apps/demoapp/views.py
@@ -8,10 +8,11 @@ from monzohosting import models
 # Create your views here.
 class IndexView(TemplateView):
     template_name = "index.html"
+
     def get_context_data(self, **kwargs):
         monzo = models.Settings.get_monzo()
         accounts = monzo.accounts()
         c = {}
         for account in accounts:
-            c[account.id] = monzo.balance(account.id)
-        return c
+            c[account.id] = monzo.balance(account.id).balance
+        return {'accounts': c}
